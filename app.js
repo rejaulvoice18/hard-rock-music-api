bfae9ff0bfbef107047c3e567e4e74d4cb28d8da
Rejaul Karim <rejaulvoice@gmail.com>
2024-01-16 12:38:52 +0000

    2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+//Enter key er maddhome search button a click koranu hochhe nicher function er madhome
+
+document.getElementById('input-value').addEventListener("keypress", function (event){
+    if(event.key === 'Enter'){
+        document.getElementById("search-button").click();
+    }
+})
 const searchSongs = async() => {
     const getInput = document.getElementById('input-value').value;
     const url = `https://api.lyrics.ovh/suggest/${getInput}`
+    // toggleSpinner(true);
+    toggleSpinner();
        try{
          //Load data
          const res = await fetch(url);
@@ -10,6 +19,7 @@ const searchSongs = async() => {
        catch(error){
         displayError(error)
        }
+       
 }
 
 //Function for Display Data
@@ -34,6 +44,8 @@ const displaySongs = (lyric) => {
         </div>
         `
         lyricContainer.appendChild(singleDiv);
+        // toggleSpinner(false);
+        toggleSpinner();
     });
 }
 
@@ -60,3 +72,35 @@ const displayError = error =>{
     const errorTag = document.getElementById('error-message')
     errorTag.innerText = error;
 }
+
+// const toggleSpinner = (show) => {
+//     const spinner = document.getElementById('loading-spinner');
+//     if(show){
+//         spinner.classList.remove('d-none');
+//     }
+//     else{
+//         spinner.classList.add('d-none');
+//     }
+// }
+
+// loading-spinner tag er value initialy e (d-none) means display none rakha hoiche
+// tarpor toggleSpinner a conditionally searchSongs() function er vitor theke call kora hoiche and
+// true paramenter pass kora hoiche means call kora hoiche jar jonno sppiner theke d-none class remove kora hoiche
+// means spinner show hoiche
+// Abar toggleSpinner() data load hoye jauar por bondho korar jonno displaySongs() functionerer ekdom last er dike abar call kora hoiche
+// and parameter false pass kora hoiche, parameter false houar karone toggleSpinner abar spinner a d-none class add korche
+// d-none means display none;
+
+// Uporer kaj ta aaro short cut a kora jay ta niche dekhanu holo
+
+const toggleSpinner = () => {
+    const spinner = document.getElementById('loading-spinner');
+    // const songs = document.getElementById('lyricContainer'); // lyricContainer a o d-none class add korar mechanism add kora hoiche aikhane
+    spinner.classList.toggle('d-none');
+    // songs.classList.toggle('d-none');
+
+
+}
+
+// Ekhon toggleSpinner() function a toggle method ta use kora hoiche, jeta ekbar d-none class remove korbe
+// aarekbar d-none class ta add korebe loading-spinner tag e.
